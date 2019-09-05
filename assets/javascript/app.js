0e57ed340437d22ac585719a207db9b9083e4df8
Jandy <reverdy@protonmail.com>
2019-09-04 23:41:16 -0700

    countdown time stops at 0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,28 @@
-var number=10; //300=5 minutes
+var number = 3; //300=5 minutes
 var countdown;
 
-window.onload=run();
+window.onload = run();
 
-function run(){
-    countdown=setInterval(decrease, 1000);
+function run() {
+    countdown = setInterval(decrease, 1000);
 }
 
-function decrease(){
+function decrease() {
     number--;
-        var converted = timeConverter(number);
+    var converted = timeConverter(number);
     // console.log(converted);
     $("#timer").html("<h3>" + converted + "</h3>");
+
+    if (number === 0) {
+        timeIsUp();
+        console.log("TODO: reveal page");
+    }
 }
 
-  
+function timeIsUp() {
+    clearInterval(countdown);
+    // number=10;
+}
 
 // setTimeout(timeIsUp,10000); //10 seconds 
 // console.log(number);
@@ -25,25 +33,23 @@ function decrease(){
 
 // }
 
-function timeIsUp(){
-    console.log("TODO: reveal page");
-}
+
 
 function timeConverter(t) {
 
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
-  
+
     if (seconds < 10) {
-      seconds = "0" + seconds;
+        seconds = "0" + seconds;
     }
-  
+
     if (minutes === 0) {
-      minutes = "00";
+        minutes = "00";
     }
     else if (minutes < 10) {
-      minutes = "0" + minutes;
+        minutes = "0" + minutes;
     }
-  
+
     return minutes + ":" + seconds;
-  }
+}
